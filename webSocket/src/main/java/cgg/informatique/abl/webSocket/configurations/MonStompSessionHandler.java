a56f9b60530b40f6ad48bb3d54e4cd2081795b0f
Antoine Brassard <antoine.brassard@gmail.com>
2018-10-20 15:13:19 -0400

    Project Base

---
+++ webSocket/src/main/java/cgg/informatique/abl/webSocket/configurations/MonStompSessionHandler.java
@@ -0,0 +1,34 @@
+package cgg.informatique.abl.webSocket.configurations;
+
+import org.springframework.messaging.simp.stomp.*;
+
+import java.lang.reflect.Type;
+
+public class MonStompSessionHandler implements StompSessionHandler {
+
+
+    @Override
+    public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
+
+    }
+
+    @Override
+    public void handleException(StompSession session, StompCommand command, StompHeaders headers, byte[] payload, Throwable exception) {
+
+    }
+
+    @Override
+    public void handleTransportError(StompSession session, Throwable exception) {
+
+    }
+
+    @Override
+    public Type getPayloadType(StompHeaders headers) {
+        return null;
+    }
+
+    @Override
+    public void handleFrame(StompHeaders headers, Object payload) {
+
+    }
+}
---
+++ webSocket/src/main/java/cgg/informatique/abl/webSocket/controleurs/ReponseControleur.java
@@ -0,0 +1,25 @@
+package cgg.informatique.abl.webSocket.controleurs;
+
+
+
+        import cgg.informatique.abl.webSocket.Message;
+        import cgg.informatique.abl.webSocket.Reponse;
+        import org.springframework.messaging.handler.annotation.MessageMapping;
+        import org.springframework.messaging.handler.annotation.SendTo;
+        import org.springframework.web.bind.annotation.CrossOrigin;
+        import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReponseControleur {
+
+    static long id = 1;
+
+    @CrossOrigin()
+    @MessageMapping("/message")
+    @SendTo("/sujet/reponse")
+    public Reponse reponse(Message message) {
+        System.err.println(message.toString());
+        return new Reponse( id++, message.getDe(), message.getTexte() ,message.getCreation() , message.getAvatar());
+    }
+
+}
