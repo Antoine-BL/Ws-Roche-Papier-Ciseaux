90d8efeb6a96b70863a0b3c9ba307299e03b0f4e
Antoine Brassard <antoine.brassard@gmail.com>
2018-11-23 11:35:04 -0500

    index.html pretty much fixed

--- webSocket/src/main/java/cgg/informatique/abl/webSocket/dto/SanitizedCompte.java
+++ webSocket/src/main/java/cgg/informatique/abl/webSocket/dto/SanitizedCompte.java
@@ -2,6 +2,7 @@ package cgg.informatique.abl.webSocket.dto;
 
 import cgg.informatique.abl.webSocket.entites.Combat;
 import cgg.informatique.abl.webSocket.entites.Compte;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
@@ -10,8 +11,11 @@ import java.util.List;
 @JsonSerialize(as= SanitizedCompte.class)
 @JsonDeserialize(as= Compte.class)
 public interface SanitizedCompte extends SanitizedUser {
+    @JsonIgnore
     List<Combat> getCombatsRouge();
+    @JsonIgnore
     List<Combat> getCombatsBlanc();
+    @JsonIgnore
     List<Combat> getCombatsArbitre();
     boolean isDeshonore();
     int getPoints();
