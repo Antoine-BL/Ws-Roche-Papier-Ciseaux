2c34a4924e5de4a6119609e808d635c31205a1ff
Antoine Brassard <antoine.brassard@gmail.com>
2018-11-25 17:06:09 -0500

    fix Joindre

--- webSocket/src/main/java/cgg/informatique/abl/webSocket/messaging/commands/Joindre.java
+++ webSocket/src/main/java/cgg/informatique/abl/webSocket/messaging/commands/Joindre.java
@@ -1,6 +1,9 @@
 package cgg.informatique.abl.webSocket.messaging.commands;
 
 import cgg.informatique.abl.webSocket.game.lobby.Lobby;
+import cgg.informatique.abl.webSocket.game.lobby.LobbyPosition;
+import cgg.informatique.abl.webSocket.game.lobby.LobbyRole;
+import cgg.informatique.abl.webSocket.game.lobby.LobbyUserData;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 @JsonDeserialize
@@ -12,9 +15,9 @@ public class Joindre extends Commande{
         try {
             Lobby lobby = LobbyCommandContext.getLobby();
 
-            lobby.connect(this.getDe());
-
-            lobby.getLobbyUserData(getDe()).sentCommand();
+            LobbyUserData lud = lobby.getLobbyUserData(getDe());
+            lud.sentCommand();
+            lud.becomeRole(new LobbyPosition(LobbyRole.SPECTATEUR));
         } catch (Exception e) {
             e.printStackTrace();
             send("Échec de la connexion au lobby... Raison: " + e.getMessage(), context);
